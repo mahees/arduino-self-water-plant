d2306874995f6aac0207c3fc45c7447287b92768
Mahees I <mahees.ishaq@gmail.com>
2016-01-30 17:26:59 -0400

    logging

--- plant.ino
+++ plant.ino
@@ -9,6 +9,14 @@ Motor waterPump(2);
 DigitalPin buzzor(3);
 Logger logger(9600);
 
+//600000
+#define msReadSensorDelay 10000
+#define msBuzzorBeforeWateringDelay 100
+#define msWateringActiveDelay 2000
+#define msRetryAfterWateringDelay 15000
+#define moistLevel 550
+
+
 void setup() {
 
 }
@@ -16,8 +24,7 @@ void setup() {
 void loop() {
   takeAndprocessReading();
 
-  delay(10000); //10 seconds
-  //delay(600000); //10 min
+  delay(msReadSensorDelay);
 }
 
 
@@ -26,7 +33,7 @@ void takeAndprocessReading() {
   logger.info("taking reading: ");
   int moistureReading = moistureSensor.takeReading();
   logger.info(moistureReading);
-  if (moistureReading < 550)
+  if (moistureReading < moistLevel)
   {
     logger.info("soil moist");
     return;
@@ -34,12 +41,12 @@ void takeAndprocessReading() {
 
   logger.info("soil dry");
   logger.info("buzzor toggle");
-  buzzor.toggle(100);
+  buzzor.toggle(msBuzzorBeforeWateringDelay);
   logger.info("buzzor toggle");
   logger.info("pump activate");
-  waterPump.activate(2000);
+  waterPump.activate(msWateringActiveDelay);
   logger.info("pump deactivate");
   logger.info("waiting to take reading again");
-  delay(15000); //15 sec
+  delay(msRetryAfterWateringDelay);
   takeAndprocessReading();
 }
